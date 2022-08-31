f8c55437b8092d2b30f0e83dad58c99a5c01a05a
YaoLi3 <liyaoo6o@gmail.com>
2022-08-31 17:02:01 +0800

    add species info

--- src/Gene.vue
+++ src/Gene.vue
@@ -59,16 +59,13 @@
     // datasets
     var GENE_DATA_URL = "http://49.235.68.146/gene_data/All_Clusters.json"
 
+    var species = require('./conf/species.js');
 
     export default {
         data(){
             return {
                 // data examples :
-                Ssamples : [ { index:1, value:"Planarian",},
-                            { index:2, value:"Zebrafish",},
-                            { index:3, value:"Salamander",},
-                            { index:4, value:"Shark",},
-                            { index:5, value:"Whale",}, ],
+                Ssamples : species,
                 samples : [ { index:1, value:"Neoblast",},
                             { index:2, value:"Neural",},
                             { index:3, value:"Cathepsin+ cell",},
